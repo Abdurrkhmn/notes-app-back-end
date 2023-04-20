504c8eaf95cd562d93d800fff7ef8a41fb298d38
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2023-04-20 16:20:41 +0300

    еуые

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const Hapi = require('@hapi/hapi');
-//const routes = require('./routes');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
- 
+
 const init = async () => {
-  const NotesService = new NotesService();
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -14,19 +13,16 @@ const init = async () => {
       },
     },
   });
- 
-  //server.route(routes); dihapus sma importny karena tidak perlu
-  await server. register({
-    plugins: notes,
+
+  await server.register({
+    plugin: notes,
     options: {
-      service: NotesService,
+      service: notesService,
     },
   });
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
-/*Berkas ini menampung kode untuk membuat, mengonfigurasi, dan menjalankan HTTP server menggunakan Hapi.
-Berkas server.js ini memiliki ketergantungan terhadap berkas routes.js.*/
